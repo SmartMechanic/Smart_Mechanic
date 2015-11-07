043fd1a51a7216ea9efb8cc2fc8244217f5d60e0
Mary e. Kingsbury <mary.kingsbury@mavs.uta.edu>
2015-11-07 16:19:40 -0600

    starting database class

--- app/src/main/java/com/example/smart_mechanic/smartmechanic/DatabaseContract.java
+++ app/src/main/java/com/example/smart_mechanic/smartmechanic/DatabaseContract.java
@@ -13,12 +13,13 @@ import android.provider.BaseColumns;
 public final class DatabaseContract {
     private static final String DATABASE_NAME = "CarSounds.db";
     private static final int DBASE_VERSION = 1;
-    private static final String TEXT_TYPE = "TEXT";
-    private static final String DOUBLE_TYPE = "DOUBLE";
-    private static final String INT_TYPE = "INTEGER";
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String LONG_TYPE = " LONG";
+    private static final String DOUBLE_TYPE = " DOUBLE";
+    private static final String INT_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
 
-    private static abstract class CarSoundsEntries implements BaseColumns {
+    private static abstract class CarTypeEntries implements BaseColumns {
         public static final String TABLE_NAME = "Car";
         public static final String _ID = "ID";
         public static final String MAKE = "Make";
@@ -27,4 +28,18 @@ public final class DatabaseContract {
         public static final String POSITION = "Position";
         public static final String[] ALLCOLUMNS = {_ID,MAKE,MODEL,YEAR,POSITION};
     }
+    private static final String SQL_CREATE_CAR_TABLE =
+            "CREATE TABLE " + CarTypeEntries.TABLE_NAME + " (" +
+                    CarTypeEntries._ID + " INTEGER PRIMARY KEY," +
+                    CarTypeEntries.MAKE + TEXT_TYPE + COMMA_SEP +
+                    CarTypeEntries.MODEL + TEXT_TYPE + COMMA_SEP +
+                    CarTypeEntries.YEAR + INT_TYPE + COMMA_SEP +
+                    CarTypeEntries.POSITION + INT_TYPE +
+                    ");";
+
+    private final Context context;
+
+    private DatabaseHelper myDBHelper;
+
+
 }
